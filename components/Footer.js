0b06577e421e55ff0cabea507d08a2c551065f32
dasveloper <28981975+dasveloper@users.noreply.github.com>
2022-06-10 16:51:07 -0400

    Contact

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,9 @@ export default function Footer() {
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
-      <div className="max-w-7xl mx-auto py-8 px-6 border-t border-gray-200">
-        <p className="text-base text-gray-500 text-center">&copy; 2022 Colorwaze. All rights reserved.</p>
+      <div className="max-w-7xl mx-auto py-8 px-6 border-t border-gray-200 flex justify-between">
+        <p className="text-base text-gray-500">&copy; 2022 Colorwaze. All rights reserved.</p>
+        <a className="text-base text-gray-500" href="mailto:colorwazeapp@gmail.com">Contact us</a>
       </div>
     </footer>
   );
